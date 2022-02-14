fa71c5bcac894537e8bf2b319eae73503b44e4da
dynamo58 <sailorfirex@seznam.cz>
2022-02-14 07:12:47 +0100

    xd

---
+++ src/main.rs
@@ -0,0 +1,10 @@
+// https://crates.io/crates/actix-web
+// https://github.com/actix/examples/tree/master/database_interactions/pg
+
+// https://www.youtube.com/watch?v=PMa2m0Fe-Q4
+
+// https://rust-lang-nursery.github.io/rust-cookbook/cryptography/hashing.html
+// https://crates.io/crates/bcrypt
+fn main() {
+    println!("Hello, world!");
+}
